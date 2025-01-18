49f2cd963304c4ae270f18a830dfd6273e18d49c
bonheurNE07 <bonheurndezenc@gmail.com>
2025-01-19 00:08:52 +0200

    Prepare backend for Render deployment

--- wallet_backend/settings.py
+++ wallet_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-#5^uxtto4gn)s9j6ed4+^)^umas&%7%*xiew0(&ep0lcu8oeyh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
