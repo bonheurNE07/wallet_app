97f740e7dc83551ee3d61d9ddce6b646c79a3e7c
bonheurNE07 <bonheurndezenc@gmail.com>
2025-02-13 15:18:12 +0200

    management

--- wallet-fronted/src/services/api.js
+++ wallet-fronted/src/services/api.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 
 // create an axios instance with the base url for thr backend API
 const api = axios.create({
-    baseURL: 'https://wallet-app-1mdo.onrender.com/api/',
+    // baseURL: 'https://wallet-app-1mdo.onrender.com/api/', http://127.0.0.1:8000/
+    baseURL: 'http://127.0.0.1:8000/api/',
 });
 
 // Add a resquest interceptor to include the authorization token in every request 
